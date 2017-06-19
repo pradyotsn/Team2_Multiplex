435c2397c5e6a04b31a3a28618855d3fdad6f576
pradyotsn <pradyotsn@gmail.com>
2017-06-19 10:00:35 +0530

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -2,7 +2,7 @@ package com.proj.beans;
 
 public class Ticket {
 
-	private static int ticketId=0; 
+	private int ticketId=0; 
 	private String mobileNo;
 	private String movieName;
 	private int screenNo;
@@ -10,60 +10,75 @@ public class Ticket {
 	private String slot;
 	private int seats;
 	private String bookingDate;
-	public static int getTicketId() {
+
+	public int getTicketId() {
 		return ticketId;
 	}
-	public static void setTicketId(int ticketId) {
-		Ticket.ticketId = ticketId;
+
+	public void setTicketId(int ticketId) {
+		this.ticketId = ticketId;
 	}
+
 	public String getMobileNo() {
 		return mobileNo;
 	}
+
 	public void setMobileNo(String mobileNo) {
 		this.mobileNo = mobileNo;
 	}
+
 	public String getMovieName() {
 		return movieName;
 	}
+
 	public void setMovieName(String movieName) {
 		this.movieName = movieName;
 	}
+
 	public int getScreenNo() {
 		return screenNo;
 	}
+
 	public void setScreenNo(int screenNo) {
 		this.screenNo = screenNo;
 	}
+
 	public String getShowDate() {
 		return showDate;
 	}
+
 	public void setShowDate(String showDate) {
 		this.showDate = showDate;
 	}
+
 	public String getSlot() {
 		return slot;
 	}
+
 	public void setSlot(String slot) {
 		this.slot = slot;
 	}
+
 	public int getSeats() {
 		return seats;
 	}
+
 	public void setSeats(int seats) {
 		this.seats = seats;
 	}
+
 	public String getBookingDate() {
 		return bookingDate;
 	}
+
 	public void setBookingDate(String bookingDate) {
 		this.bookingDate = bookingDate;
 	}
+
 	@Override
 	public String toString() {
-		return "Ticket [mobileNo=" + mobileNo + ", movieName=" + movieName + ", screenNo=" + screenNo + ", showDate="
-				+ showDate + ", slot=" + slot + ", seats=" + seats + ", bookingDate=" + bookingDate + "]";
+		return "Ticket [ticketId=" + ticketId + ", mobileNo=" + mobileNo + ", movieName=" + movieName + ", screenNo="
+				+ screenNo + ", showDate=" + showDate + ", slot=" + slot + ", seats=" + seats + ", bookingDate="
+				+ bookingDate + "]";
 	}
-	
-	
-	
-	}
+}
